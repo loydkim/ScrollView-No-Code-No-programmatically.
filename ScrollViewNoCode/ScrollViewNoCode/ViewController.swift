df34345af783b582f04cd5aa145a086e855b258c
Loydkim <kimloydmail@gmail.com>
2018-09-16 17:44:25 -0700

    Add files via upload

---
+++ ScrollViewNoCode/ScrollViewNoCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollViewNoCode
+//
+//  Created by YOUNGSIC KIM on 2018-09-16.
+//  Copyright © 2018 YOUNGSIC KIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
